63255f83274b42cecd03bd095b9c31cfed9b4a00
Eduardo Perez <prz.rmz.eduardo@gmail.com>
2023-12-08 14:21:18 -0600

    TEST Docker 1

---
+++ Dockerfile
@@ -0,0 +1,51 @@
+## Etapa 1 instalacion 
+FROM node:alpine as dev
+WORKDIR /app
+# Copiar los archivos de configuración
+COPY package.json pnpm-lock.yaml ./
+# Instalar dependencias (todas)
+RUN npm install -g pnpm
+RUN pnpm install
+# Copiar codigo fuente
+#COPY . .
+RUN pnpm run start:dev
+
+
+
+# Etapa 2. Dependencias de desarrollo
+FROM node:alpine as dev-deps
+WORKDIR /app
+# Copiar los archivos de configuración
+COPY package.json pnpm-lock.yaml ./
+# Instalar dependencias (todas)
+RUN npm install -g pnpm
+RUN pnpm install
+
+
+
+# Etapa 3. Construir build
+FROM node:alpine as build
+WORKDIR /app
+# Copiar los archivos de configuración
+COPY --from=dev-deps /app/node_modules ./node_modules
+COPY . .
+RUN npm install -g pnpm
+RUN pnpm run build
+
+
+# Etapa 4. Depenedencias de produccion
+FROM node:alpine as prod-deps
+ENV NODE_ENV production
+WORKDIR /app
+COPY package.json pnpm-lock.yaml ./
+RUN npm install -g pnpm
+RUN pnpm install --production 
+
+
+# Etapa 5 Crear la imagen final ligera
+FROM node:alpine as prod
+EXPOSE 3000
+WORKDIR /app
+COPY --from=prod-deps /app/node_modules ./node_modules
+COPY --from=build /app/dist ./dist
+CMD ["node", "dist/main.js"]
--- src/main.ts
+++ src/main.ts
@@ -2,34 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
-import {readFileSync } from 'fs';
+import { readFileSync } from 'fs';
 import { ServerCredentials } from '@grpc/grpc-js';
 
 async function bootstrap() {
-
   const credentials = ServerCredentials.createSsl(
-    readFileSync(join(__dirname, 'certs', 'ca.crt')), [
-    {
-      cert_chain: readFileSync(join(__dirname, 'certs', 'server.crt')),
-      private_key: readFileSync(join(__dirname, 'certs', 'server.key'))
-    }
-  ], true
+    readFileSync(join(__dirname, 'certs', 'ca.crt')),
+    [
+      {
+        cert_chain: readFileSync(join(__dirname, 'certs', 'server.crt')),
+        private_key: readFileSync(join(__dirname, 'certs', 'server.key')),
+      },
+    ],
+    true,
   );
-  
-  
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       logger: ['warn', 'error'],
       transport: Transport.GRPC,
-      options:{
-        url: '0.0.0.0:9000',
+      options: {
+        url: '0.0.0.0:3000',
         package: 'db',
         protoPath: join(__dirname, './main.proto'),
         credentials,
-        maxSendMessageLength: 1024 * 1024 * 20
+        maxSendMessageLength: 1024 * 1024 * 30,
       },
-    }
+    },
   );
   await app.listen();
 }
